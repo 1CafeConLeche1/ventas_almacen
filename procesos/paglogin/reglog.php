128ab83624d59ad498e7149b5c910c8994a22e99
Marlon Vásquez H <mvhsiesa@gmail.com>
2020-11-13 08:17:17 -0600

    Se agregó documentación al proyecto

--- procesos/paglogin/reglog.php
+++ procesos/paglogin/reglog.php
@@ -1,4 +1,6 @@
 <?php
+
+//Este código procesa la información que viene del formulario registro usuario
 require_once "../../clases/Usuario.php";
 $usuario = new Usuario();
 $datos = array(
@@ -8,5 +10,6 @@ $_POST["email"],
 $_POST["password"]
 );
 
+//Se envían los datos al modelo para almacenarlos en la base de datos 
 echo $usuario->registrarUsuario($datos);
 ?>
