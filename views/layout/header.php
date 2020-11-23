4f401194f7e9f2a8b394d337aa3ebac31bf7d0da
Brandillon <branditoncr@gmail.com>
2020-11-23 21:00:01 +0100

    Estilotes

--- views/layout/header.php
+++ views/layout/header.php
@@ -3,12 +3,16 @@
 <html lang="en">
 
 <head>
-    <link rel="icon" href="<?=base_url?>assets/img/logo_carrito.png" type="image/gif" sizes="16x16">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="icon" href="<?= base_url ?>assets/img/logo_carrito.png" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas Almacén</title>
+    <link rel="stylesheet" href="<?=base_url?>assets/css/style.css">
     <link rel="stylesheet" href="<?= base_url ?>assets/css/styles.css">
 </head>
 
